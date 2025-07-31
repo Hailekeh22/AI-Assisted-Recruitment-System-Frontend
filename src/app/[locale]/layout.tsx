71bd158ac5365ee28021669302c6798b2e663ea4
Hailekeh22 <hailekjt@gmail.com>
2025-07-31 19:15:26 +0300

    redux setup

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,25 +1,29 @@
-import {NextIntlClientProvider, hasLocale} from 'next-intl';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
-import '@/app/globals.css';
- 
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import "@/app/globals.css";
+import { ReduxProvider } from "./reduxProvider";
+
+
 export default async function LocaleLayout({
   children,
-  params
+  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: Promise<{ locale: string }>;
 }) {
   // Ensure that the incoming `locale` is valid
-  const {locale} = await params;
+  const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
- 
+
   return (
     <html lang={locale}>
       <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider>
+          <ReduxProvider>{children}</ReduxProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
