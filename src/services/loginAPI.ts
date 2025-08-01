3708758a4836e9ac7f552385742ef3d3e10ff897
Hailekeh22 <hailekjt@gmail.com>
2025-08-01 13:12:12 +0300

    api fix

--- src/services/loginAPI.ts
+++ src/services/loginAPI.ts
@@ -1,17 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const loginApi = createApi({
-    reducerPath: 'loginApi',
-    baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_USERS_API_URL}),
-    endpoints: (builder) => ({
-        loginUser: builder.mutation({
-            query: (userData) => ({
-                url: '/login',
-                method: 'POST',
-                body: userData
-            })
-        })
-    })
-})
+  reducerPath: "loginApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_USERS_API_URL,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    loginUser: builder.mutation({
+      query: (userData) => ({
+        url: "/login",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+  }),
+});
 
-export const { useLoginUserMutation} = loginApi;
+export const { useLoginUserMutation } = loginApi;
