09234fe8a640d021be994095606ba436a9918239
Hailekeh22 <hailekjt@gmail.com>
2025-08-22 10:41:18 +0300

    roll back

--- src/services/authAPI.ts
+++ src/services/authAPI.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "loginApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_USERS_API_URL,
+    baseUrl: process.env.NEXT_PUBLIC_USERS_API_URL, 
     credentials: "include",
   }),
   endpoints: (builder) => ({
